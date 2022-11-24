7d293343a151a55e3a0e3c709a6e5dd03ed853bb
“Mehmet <70447221+harunapak@users.noreply.github.com>
2022-11-25 00:07:21 +0100

    3.video

--- src/test/java/video1_mavenProjeOlusturma/Class1.java
+++ src/test/java/video1_mavenProjeOlusturma/Class1.java
@@ -4,11 +4,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class Class1 {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver= new ChromeDriver();
         driver.get("https://amazon.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
 
 
